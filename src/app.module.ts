a57d65fbd6f5bd814f55d84058c482b2b95ded1b
= <pmosadeqzadeh@gmail.com>
2025-04-10 00:05:15 +0330

    chore: imported orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { UsersController } from './users/users.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [UsersModule, ReportsModule, TypeOrmModule],
   controllers: [UsersController, ReportsModule],
 })
 export class AppModule {}
