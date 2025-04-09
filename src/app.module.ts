a1955bcec955c9b321418c3d8998c70fea6be13c
= <pmosadeqzadeh@gmail.com>
2025-04-10 00:08:52 +0330

    chore: created database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,17 @@ import { ReportsModule } from './reports/reports.module';
 import { UsersController } from './users/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      // * Will be filled later
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [UsersController, ReportsModule],
 })
 export class AppModule {}
